fbb4fd040bc596125695d408353266a65aaa5c8f
Jason Chilcott <jasonchilcott@me.com>
2020-08-09 20:49:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  labeled_hash = {}
+  emoticons.each do |key, value|
+    # for each key, if it doesnt exist in labeled hash, create it, key value for each.
+    labeled_hash[key] = {}
+    labeled_hash[key][:english] = value[0]
+    labeled_hash[key][:japanese] = value[1]
+  end
 end
 
 def get_japanese_emoticon
